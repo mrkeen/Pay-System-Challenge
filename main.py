b2cadaadfff0458916acaaf0830f67c4f0553c03
EdwardKeen <51882903+mrkeen@users.noreply.github.com>
2022-02-17 16:14:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+hours = float(input("What is the total number of hours that you work weekly?"))
+overtime = float(input("What is the total number of overtime hours that you work weekly?"))
+payScale = int(input("Enter your pasyscale"))
+
+
+print("The total number of hours that you work in a week is: " , hours+overtime)
